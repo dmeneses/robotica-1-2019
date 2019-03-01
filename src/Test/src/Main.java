08be2f3447c0278ff022dd4a3ab530757f76a0b6
Estudiante Robotica <roboticaest@b204-04.fundacion-jala.local>
2019-03-01 09:49:06 -0400

    Proyecto inicial de testeo

---
+++ src/Test/src/Main.java
@@ -0,0 +1,13 @@
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		LCD.drawString("Plugin Test", 0, 4);
+		Delay.msDelay(5000);
+
+	}
+
+}
