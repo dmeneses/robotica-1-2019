1ca903701a1cc1b05abb217b431b16ea1c3dd997
Daniela Meneses <dvargas@oildex.com>
2019-03-11 22:09:05 -0400

    Added more scripts

--- utils/git-setup.sh
+++ utils/git-setup.sh
@@ -1,3 +1,6 @@
 # Add step to configure aliases for pull, push the code
 git config --global alias.iniciar "!sh -c 'git checkout -b \$1 && git push -u origin \$1' -"
 git config --global alias.guardarCambios "!sh -c 'git add -A && git commit -m \"\$1\" && git push' -"
+git config --global alias.abrirProyecto "!sh -c 'git checkout \$1' -"
+git config --global alias.obtenerCambios "!sh -c 'git pull' -"
+git config --global alias.verProyectos "!sh -c 'git branch' -"
