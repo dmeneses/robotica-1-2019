0d0539fe11dbf3696bbead65d29774ed5351ef2b
Estudiante Robotica <roboticaest@b204-05.fundacion-jala.local>
2019-03-13 14:37:37 -0400

    primera clase

--- src/Test/src/Main.java
+++ src/Test/src/Main.java
@@ -1,13 +1,28 @@
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.utility.Delay;
 
 
 public class Main {
 
 	public static void main(String[] args) {
-		LCD.drawString("Plugin Test", 0, 4);
-		Delay.msDelay(5000);
+		//LCD.drawString("Plugin Test", 0, 4);
+		//Delay.msDelay(5000);
+		EV3LargeRegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.A);
+        EV3LargeRegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.B);
+        
+        motorA.setSpeed(500);
+        motorA.forward();    // starts rotation.
+
+        //motorA.setAcceleration(500);
+        motorB.setSpeed(500);
+        motorB.forward();    // starts rotation.
+        
+     // wait 3 seconds.
+        Delay.msDelay(3000);
 
 	}
 
 }
+
