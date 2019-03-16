4ffb54bd8ef1ddbce5b499f139b68546e971f137
Estudiante Robotica <roboticaest@b204-03.fundacion-jala.local>
2019-03-16 10:03:43 -0400

    motores

---
+++ Primerito/src/Primer.java
@@ -0,0 +1,23 @@
+import lejos.hardware.Button;
+import lejos.hardware.motor.Motor;
+
+
+public class Primer
+{
+	
+	public static void main(String args[])
+	{ 
+		System.out.println("Hola Girl Gang <3");
+		int dis;
+		dis = 24;
+		//2pi*r//360/17.5*dis//
+		double perimetro = 2*Math.PI*2.75; 
+		double grados = 360/perimetro*dis;
+		
+		
+		Motor.B.rotate((int)grados, true);
+		Motor.C.rotate((int)grados);
+		
+		Button.waitForAnyPress();
+	}
+}
