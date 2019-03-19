35e52ea01327329ccd9267c3de520a0e0cbd33d6
Estudiante Robotica <roboticaest@b204-01.fundacion-jala.local>
2019-03-19 16:02:22 -0400

    mario

---
+++ src/variables/src/variables/variables_prueba.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class variables_prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+int numero1 = 20;
+int numero2 = 40;
+int resultado = numero1 + numero2; 
+System.out.println(resultado);
+
+
+String saludo;
+saludo = 
+	}
+
+}
