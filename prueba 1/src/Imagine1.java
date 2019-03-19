6146b2a51d12fe4fd15f3997a9a91275b88830ca
Estudiante Robotica <roboticaest@b204-06.fundacion-jala.local>
2019-03-19 18:03:42 -0400

    Imagine

--- prueba 1/src/Imagine1.java
+++ prueba 1/src/Imagine1.java
@@ -1,25 +1,22 @@
 import lejos.hardware.Button;
 import lejos.hardware.motor.Motor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
 
 
 public class Imagine1 
 {
      public static void main(String args[])
   {
-    	 int dis;
-	     dis=30;
-	 int grad;
-	 grad = dis * 36;  
-		  
-    	 Motor.B.setSpeed(400);
-    	 Motor.C.setSpeed(400);
-    	 Motor.B.rotate(grad,true);
-    	 Motor.C.rotate(grad);
-    	 Motor.C.rotate(720);
-    	 Motor.B.rotate(grad,true);
-    	 Motor.C.rotate(grad);
-    	 
-    	 System.out.println("hola mundo");
+    EV3UltrasonicSensor ojos;
+    ojos = new EV3UltrasonicSensor(SensorPort.S2);
+    float[]muestras = new float[ojos.sampleSize()];
+    ojos.fetchSample(muestras, 0);
+    System.out.println("distancia--->" + muestras[0]);	
+    float[] distancia;
+    distancia = muestras;
+	Motor.B.rotate(distancia,true);
+	Motor.C.rotate(distancia);
     	 
     	 Button.waitForAnyPress();
     	    
