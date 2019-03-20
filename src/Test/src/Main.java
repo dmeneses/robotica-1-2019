a8fb7b3834b5ea329a37200cb62470b2288d0281
Estudiante Robotica <roboticaest@b204-10.fundacion-jala.local>
2019-03-20 16:03:32 -0400

    robot cuadrado

--- src/Test/src/Main.java
+++ src/Test/src/Main.java
@@ -1,12 +1,36 @@
-import lejos.hardware.lcd.LCD;
+import lejos.hardware.Button;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.utility.Delay;
 
-
-public class Main {
+public class pruebaabanzar {
 
 	public static void main(String[] args) {
-		LCD.drawString("Plugin Test", 0, 4);
+		EV3LargeRegulatedMotor motorB = new  EV3LargeRegulatedMotor(MotorPort.B);
+		EV3LargeRegulatedMotor motorC = new  EV3LargeRegulatedMotor(MotorPort.C);
+		
+	    motorB.setSpeed(200);
+		motorB.forward();
+		
+		motorC.setSpeed(200);
+		motorC.forward();
+		
 		Delay.msDelay(5000);
+		
+		/*motorB.stop();
+		motorC.stop();
+		motorC.setSpeed(500);
+		motorB.setSpeed(500);
+		
+		//Button.waitForAnyPress();
+		 
+		double distancia = 30;
+		int grados = (int)distancia*360/17;
+		motorB.rotate(grados,true);
+		motorC.rotate(grados);
+		
+		motorB.close();
+		motorC.close();*/
 
 	}
 
--- src/robot avansado/src/pruebaabanzar.java
+++ src/robot avansado/src/pruebaabanzar.java
@@ -9,10 +9,10 @@ public class pruebaabanzar {
 		EV3LargeRegulatedMotor motorB = new  EV3LargeRegulatedMotor(MotorPort.B);
 		EV3LargeRegulatedMotor motorC = new  EV3LargeRegulatedMotor(MotorPort.C);
 		
-		/*motorB.setSpeed(200);
+		/*motorB.setSpeed(800);
 		motorB.forward();
 		
-		motorC.setSpeed(200);
+		motorC.setSpeed(800);
 		motorC.forward();
 		
 		Delay.msDelay(5000);
@@ -20,21 +20,124 @@ public class pruebaabanzar {
 		motorB.stop();
 		motorC.stop();
 		motorC.setSpeed(500);
-		motorB.setSpeed(500);*/
+		motorB.setSpeed(500);
 		
-		//Button.waitForAnyPress();
+		Button.waitForAnyPress();
 		 
-		double distancia = 30;
+		double distancia = 50;
 		int grados = (int)distancia*360/17;
 		motorB.rotate(grados,true);
 		motorC.rotate(grados);
 		
-		/*motorB.close();
-		motorC.close();*/
+		Button.waitForAnyPress();
+		
+		motorB.setSpeed(700);
+		motorB.rotate(grados);
+		
+	    /*distancia = 200;
+		grados = (int)distancia*360/17;
+		motorB.rotate(grados);
+		motorC.rotate(grados);*/
+		
+		/*sadouble distancia = 57;
+		int grados = (int)distancia*360/17;
+		motorB.rotate(grados,true);
+		motorC.rotate(grados);
 		
 		
+		Button.waitForAnyPress();
+		
+		distancia = 0;
+	    grados = 400;
+		motorB.rotate(grados);
+		
+		Button.waitForAnyPress();
+		
+		distancia = 57;
+		grados = (int)distancia*360/17;
+		motorB.rotate(grados,true);
+		motorC.rotate(grados);
 		
 		
+		Button.waitForAnyPress();
+		
+		distancia = 0;
+	    grados = 400;
+		motorB.rotate(grados);
+		
+		Button.waitForAnyPress();
+		
+		distancia = 57;
+		grados = (int)distancia*360/17;
+		motorB.rotate(grados,true);
+		motorC.rotate(grados);
+		
+		
+		Button.waitForAnyPress();
+		
+		distancia = 0;
+	    grados = 400;
+		motorB.rotate(grados);
+		
+		Button.waitForAnyPress();
+		
+		distancia = 57;
+		grados = (int)distancia*360/17;
+		motorB.rotate(grados,true);
+		motorC.rotate(grados);
+		
+		
+		Button.waitForAnyPress();
+		
+		distancia = 0;
+	    grados = 400;
+		motorB.rotate(grados);
+		
+		Button.waitForAnyPress();
+		
+	    distancia = 57;
+	    grados = (int)distancia*360/17;
+		motorB.rotate(grados,true);
+		motorC.rotate(grados);sa*/
+		
+		Button.waitForAnyPress();
+		
+		double distancia = 59;
+		int grados = (int)distancia*360/17;
+		motorB.rotate(grados,true);
+		motorC.rotate(grados);
+		
+	    distancia = 17;
+	    grados = (int)distancia*360/17;
+		motorB.rotate(grados);
+		
+		distancia = 59;
+	    grados = (int)distancia*360/17;
+		motorB.rotate(grados,true);
+		motorC.rotate(grados);
+		
+	    distancia = 17;
+	    grados = (int)distancia*360/17;
+		motorB.rotate(grados);
+		
+		distancia = 59;
+	    grados = (int)distancia*360/17;
+		motorB.rotate(grados,true);
+		motorC.rotate(grados);
+		
+	    distancia = 17;
+	    grados = (int)distancia*360/17;
+		motorB.rotate(grados);
+		
+		distancia = 59;
+	    grados = (int)distancia*360/17;
+		motorB.rotate(grados,true);
+		motorC.rotate(grados);
+		
+	    distancia = 17;
+	    grados = (int)distancia*360/17;
+		motorB.rotate(grados);
+	
 		
 		
 		
