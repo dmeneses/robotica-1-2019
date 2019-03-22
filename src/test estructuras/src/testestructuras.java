f9cef3dc9ca898badbcadae1d1d5cdfd53701e42
Estudiante Robotica <roboticaest@b204-09.fundacion-jala.local>
2019-03-22 16:04:31 -0400

    robot sensor touch

---
+++ src/test estructuras/src/testestructuras.java
@@ -0,0 +1,54 @@
+
+public class testestructuras {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int condicion;/*
+condicion = 9;
+
+if(condicion >10){
+	System.out.println("la variable es mayor");
+	}
+	
+else
+{
+	System.out.println("la variable es menor");
+	}
+
+}
+{
+	for (int i= 0 ; i<5 ;i++)
+	{
+	System.out.println("repeticion numero:"+ i);
+	}
+	for (int i= 0 ; i<11 ;i = i +2)
+	{
+	System.out.println("repeticion numero:"+ i);
+	}
+	int y=0;
+	while( y != 5)
+	{
+		System.out.println("repeticion numero:"+ y);
+		y++;
+	}
+	int x =0;
+	do{
+		System.out.println("repeticion numero:"+ x);
+	
+	}while (x != 5);*/
+	
+	String [] listadecompras =new String [5];
+	 listadecompras [0] = "pollo";
+	 listadecompras [1] ="refresco";
+	 listadecompras [2] ="pudin";
+
+	 listadecompras [3] ="leche";
+	 listadecompras [4] ="cordones";
+	
+	
+	 for (int i= 0 ; i<5 ;i++)
+		{
+		System.out.println(listadecompras[i]);
+		}
+}
+}
