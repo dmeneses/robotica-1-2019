f860b8f430054875017b4d311a85ebe8eba7e55e
Estudiante Robotica <roboticaest@b204-06.fundacion-jala.local>
2019-03-26 18:04:43 -0400

    dod

--- prueba 1/src/Imagine1.java
+++ prueba 1/src/Imagine1.java
@@ -18,17 +18,17 @@ public class Imagine1
     EV3TouchSensor pulsador ;
     pulsador = new EV3TouchSensor(SensorPort.S1);
     float []muestrasPulsador = new float[pulsador.sampleSize()];
-
-    
-    
+    System.out.println("Listo");
+    while(true)
+    {
 	    pulsador.fetchSample(muestrasPulsador, 0);	
-	    System.out.println("pulsador "+ muestrasPulsador[0]);
-	
+	    
+	    System.out.println( "Pulsador" +muestrasPulsador[0] );
 	    ojos.fetchSample(muestras, 0);	
-
+	    
 	    double distancia = muestras[0]*100;
 	    
-	    System.out.println("distancia "+ distancia);
+	    
 
 	    //avanzar en centimetros
 	    
@@ -36,14 +36,18 @@ public class Imagine1
 	    //double perimetro = 2 * 3.1416 * 3;
 	    //regla tres 360 ----- perimetro
 	    //			 ?   ----- distancia
-	    
+	    System.out.println("Distancia" +  muestras[0] );
 	    //?
-	    double grados = distancia * 18.40; 
-	    Motor.B.setSpeed(50);
-	    Motor.C.setSpeed(50);
-	    Motor.B.rotate((int)grados, true);
-	    Motor.C.rotate((int)grados);
-	    Button.waitForAnyPress();
+	    double grados = distancia * 18.80; 
+	    Motor.B.setSpeed(350);
+	    Motor.C.setSpeed(350);
+	    if(muestrasPulsador[0] == 1)
+	    {	
+	      Motor.B.rotate((int)grados, true);
+	      Motor.C.rotate((int)grados);
+	      
+	    }	    
+       }
       }  
     }
 
