072cfeba1c98404aec37364eb0ddb14b989c95de
Estudiante Robotica <roboticaest@b204-06.fundacion-jala.local>
2019-03-30 10:02:46 -0400

    yo

---
+++ Primero/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Conejo boxbunny;
+		boxbunny = new Conejo();
+		boxbunny.despierto = true;
+		boxbunny.edad = 20;
+		boxbunny.genero = "masculino";
+		boxbunny.nombre = "El box";
+		boxbunny.dormir();
+			
+
+	}
+
+}
