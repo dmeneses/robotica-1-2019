908869b6c286cb5e7e155ed71067f9d91e34c923
Estudiante Robotica <roboticaest@b204-19.fundacion-jala.local>
2019-03-30 10:04:33 -0400

    asd

---
+++ primer/src/charanko.java
@@ -0,0 +1,37 @@
+public class charanko {
+//programacion orientada a objetos V:
+	public static  void main(String[] args){
+	lapiz lapiz1;
+	lapiz1=new lapiz();
+	lapiz1.forma ="hexagonal";
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+  
