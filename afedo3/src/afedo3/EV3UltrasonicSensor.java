f8f42aeda322939662b0f7cdeda67bc9c919ee31
Estudiante Robotica <roboticaest@b204-13.fundacion-jala.local>
2019-04-06 10:00:27 -0400

    motores

--- afedo3/src/afedo3/EV3UltrasonicSensor.java
+++ afedo3/src/afedo3/EV3UltrasonicSensor.java
@@ -2,4 +2,9 @@ package afedo3;
 
 public class EV3UltrasonicSensor {
 
+	public void fetchSample(Object muestraOjos, int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- afedo3/src/afedo3/inicio.java
+++ afedo3/src/afedo3/inicio.java
@@ -4,6 +4,8 @@ public class inicio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Pato pato1;
+		
 
 	}
 
