ef6b76f36c987e9957624125299e2de3bc752b6b
Estudiante Robotica <roboticaest@b204-09.fundacion-jala.local>
2019-04-12 16:04:01 -0400

    12/4/19

---
+++ src/Test/src/.metadata/.plugins/org.eclipse.core.resources/.history/ee/208002bc535d0019168e9ea262dc39bd
@@ -0,0 +1,9 @@
+
+public class GirodeEje {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Test/src/Postas2/src/Robot.java
@@ -0,0 +1,10 @@
+import lejos.hardware.motor.EV3largeRegulatedMotor;
+public class Robot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
