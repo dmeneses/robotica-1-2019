4aab1125fc8c1bc2c14d117a0a9df8b609c8a8b5
Estudiante Robotica <roboticaest@b204-07.fundacion-jala.local>
2019-04-16 15:58:38 -0400

    cambios fury

--- src/Mate-Said/Sensor de color/src/Sensor.java
+++ src/Mate-Said/Sensor de color/src/Sensor.java
@@ -3,8 +3,33 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
 public class Sensor {
 	public static void main(String[] args) {
-		Robot Fury = new Robot ("Fury");
-		String nombredelrobot = Fury.nombre;
-		System.out.println(Fury.nombre);
-}
+		EV3ColorSensor C = new  EV3ColorSensor(SensorPort.S4);
+		SampleProvider sp = C.getColorIDMode();
+	    String Color; 
+	    Color = null;
+		 while(true){
+	        	float[] sample = new float [sp.sampleSize()];
+	        	sp.fetchSample(sample, 0);
+	        	if(sample[0]==0)
+	        	{
+	        		Color = "Rojo" ;
+	        	}
+	        	else if(sample[0]==1)
+	        	{
+	        		 Color = "Verde";
+	        	}
+	        	else if(sample[0]==2)
+	        	{
+	        		 Color = "Azul";
+	        	}
+	        	else if(sample[0]==3)
+	        	{
+	        		 Color = "Amarillo";
+	        	}
+	        	else{
+	        	System.out.println(Color = null);	
+	        	}
+	        	System.out.println(Color);
+  }
+ }
 }
