ac806d2a53b838a76909eaa37a23712505add56d
Estudiante Robotica <roboticaest@b204-13.fundacion-jala.local>
2019-04-30 18:01:43 -0400

    motores

---
+++ afedo3/src/afedo3/thefish.java
@@ -0,0 +1,21 @@
+package afedo3;
+
+import lejos.hardware.sensor.EV3TouchSensor;
+
+public class thefish 
+{
+	//propiedades
+	double radio;
+	double eje;
+	boton 
+	//float[] muestrasBoton;
+	EV3TouchSensor Boton;
+	
+	
+	//insertar simbolos por medio del "telegrafo"
+	//traducir el codigo insertado y mostrar en español (en pantalla)
+	//escribir el codigo morse (robot)
+	
+	
+
+}
