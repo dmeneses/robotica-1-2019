da1040113511cf283f280da8bbb6aafab9be5319
Estudiante Robotica <roboticaest@b204-07.fundacion-jala.local>
2019-05-03 15:55:10 -0400

    cambios fury

---
+++ src/Mate-Said/.metadata/.plugins/org.eclipse.core.resources/.history/d8/f0a9ebe7cf6d001917a5fde0b9c5b35f
@@ -0,0 +1,8 @@
+public class Mateogay {
+
+	public static void main(String[] args) {
+		morse fury = new morse();
+		fury.leerCodigo();	
+	}
+
+}
--- src/Mate-Said/codigo morse/src/Mateogay.java
+++ src/Mate-Said/codigo morse/src/Mateogay.java
@@ -1,5 +1,7 @@
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.SampleProvider;
 public class Mateogay {
-
 	public static void main(String[] args) {
 		morse fury = new morse();
 		fury.leerCodigo();	
