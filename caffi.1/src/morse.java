bb8a1157d37df08e3598ca082660016046804628
Estudiante Robotica <roboticaest@b204-10.fundacion-jala.local>
2019-05-03 13:16:13 -0400

    3/05/19

--- caffi.1/src/morse.java
+++ caffi.1/src/morse.java
@@ -1,4 +1,25 @@
 
 public class morse {
+	
+
+       /for (int x=0;x<s.length();x++)
+        System.out.println("Caracter " + x + ": " + s.charAt(x));
+        public static void main (String[]args){
+            System.out.println("Introdusca una Cadena");
+            String entradaTeclado = "";
+            Scanner entradaEscanner = new Scanner(System.in);
+            entradaTeclado = entradaEscanner.nextLine();
+            int m=0;
+            for(int i=0;i<entradaTeclado.length();i++){
+                System.out.println(entradaTeclado.substring(m,i+1));
+                m++;
+                for (int x=0;x<s.length();x++)
+                    System.out.println("Caracter " + x + ": " + s.charAt(x));*/
+	public static void main(String args[])
+	  telegrafo = []
+	double marcador = [];
+	int radio
+	public mors
+{
 
 }
