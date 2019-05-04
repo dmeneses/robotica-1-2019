5a9a4b187086796e3613833a695f0b2bdf3a35c3
Estudiante Robotica <roboticaest@b204-03.fundacion-jala.local>
2019-05-04 10:04:00 -0400

    letra

--- Robot/src/Pruebas.java
+++ Robot/src/Pruebas.java
@@ -5,29 +5,18 @@ public class Pruebas {
 
 	public static void main(String[] args)
 	{
-		Telegrafo telegrafo1;
-		int t = 1;
+		Letra letra1;
+		String palabra = "";
+		letra1 = new Letra(1);
 		
-		telegrafo1 = new Telegrafo();
-		String letra = "";
-		while(t==1)
-		{
-		
-		int simbolo = telegrafo1.insertarmorse();
-		
-		System.out.println("Sim: " + simbolo);
-		
-		letra = letra + "" + simbolo;
-		
-		Button.waitForAnyPress();
-		
-		
-		if (simbolo == 3)
-		{
-			System.out.println("Letra: " + letra);
-			
-			t=2;
-		}
+		letra1.insertarletra(1, "");
+		palabra = letra1.insertarletra(2, "");
+		System.out.println(palabra);
+		System.out.println("Siguiente");
+		letra1.insertarletra(1, "");
+		palabra = letra1.insertarletra(2, "");
+		System.out.println(palabra);
+		System.out.println("Siguiente");
 		Button.waitForAnyPress();
 		/*
 		simbs 
@@ -42,7 +31,7 @@ public class Pruebas {
 		
 		"... -"
 		*/
-		}
+		
 	}
 	
 
